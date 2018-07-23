2b7362043589e80238ac9655c320c0e468a8c35c
Srinivas Tamada <srinivas@9lessons.info>
2018-07-22 22:40:24 -0400

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
