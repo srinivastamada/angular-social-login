1b91e3077b9eda57091d0197bf7acec8eb41721d
Srinivas Tamada <srinivas@9lessons.info>
2018-07-25 15:26:17 -0400

    Title change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular Social Login');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular Social Login with Facebook, Google and Linkedin');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Social Login';
+  title = 'Angular Social Login with Facebook, Google and Linkedin';
 }
